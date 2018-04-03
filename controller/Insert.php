7f6aabc0f113bc9c1a7fa04acc5acadc9a31fc70
sunilchilami <sunilchilami7@gmail.com>
2018-04-03 11:56:58 +0530

    first commit

---
+++ controller/Insert.php
@@ -0,0 +1,14 @@
+<?php
+class Controller {
+
+	public function __construct(){
+
+	}
+
+	public function index(){
+		include 'view/add.php';
+	}
+
+
+}
+?>
---
+++ view/add.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Add New</title>
+</head>
+<body>
+	<h2>Add New</h2>
+	<form action="">
+	<label>Name: </label>
+	<input type="text" name="name"> <br/>
+	<label>Course: </label>
+	<input type="text" name="course"> <br/>
+	<input type="submit" name="submit"/>
+	</form>
+</body>
+</html>
